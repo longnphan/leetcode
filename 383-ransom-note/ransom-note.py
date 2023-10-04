a862b8c6c2d999a3897308611459b2342c276683
Long Phan <67768035+longnphan@users.noreply.github.com>
2023-10-03 22:24:40 -0600

    Time: 67 ms (44.65%) | Memory: 16.5 MB (50.67%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,9 +1,19 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote) > len(magazine):
+            return False
+
+        def charMap(str):
+            dict = {}
+            for char in str:
+                dict[char] = 1 + dict.get(char, 0)
+            return dict
+
+        ransom_map = charMap(ransomNote)
+        magazine_map = charMap(magazine)
+
         for char in ransomNote:
-            if char in magazine:
-                magazine = magazine.replace(char, "", 1)
-            else:
+            if ransom_map[char] > magazine_map.get(char, 0):
                 return False
-        return True
         
+        return True
