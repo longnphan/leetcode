9a956fafafe26a6816b7b4ae6e6f42c9eecc174f
Long Phan <67768035+longnphan@users.noreply.github.com>
2023-10-15 16:21:57 -0600

    Time: 43 ms (87.35%) | Memory: 16.9 MB (93.63%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -7,13 +7,15 @@ class Solution:
         while l < r:
             while l < r and not s[l].isalnum():
                 l += 1
+
             while l < r and not s[r].isalnum():
                 r -= 1
-
+            
             if s[l] != s[r]:
                 return False
-
+            
             l += 1
             r -= 1
         return True
+
 
